e49a7ff8698e7dad46ed9f752e01f296c0de6ca8
Alec Aivazis <alec@aivazis.com>
2019-02-22 10:19:05 -0800

    moved post template dir

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require('path')
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
 
-    const blogPostTemplate = path.resolve(`src/postTemplate/index.js`)
+    const blogPostTemplate = path.resolve(`src/templates/post/index.js`)
 
     return graphql(`
         {

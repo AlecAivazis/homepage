8a4514f83fab4c2b727793b0b32458553da4ce6f
Alec Aivazis <alec@aivazis.com>
2019-02-22 02:13:54 -0800

    renamed manifest name

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
+                name: `Alec Aivazis`,
                 short_name: `starter`,
                 start_url: `/`,
                 background_color: `#663399`,
